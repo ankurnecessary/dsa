8095eeafebf101c67b499a50407b113a716e4bac
Ankur Gupta <ankurnecessary@gmail.com>
2023-06-01 12:18:23 +0930

    Binary Search: Find a peak element

---
+++ 1_binarySearch/3_find_a_peak_element.js
@@ -0,0 +1,84 @@
+// 3. Find a peak element - https://www.scaler.com/academy/mentee-dashboard/class/30364/assignment/problems/4132/?navref=cl_pb_nv_tb
+/* 
+*** Problem Description
+Given an array of integers A, find and return the peak element in it.
+An array element is considered a peak if it is not smaller than its neighbors. For corner elements, we need to consider only one neighbor.
+
+NOTE:
+
+1. It is guaranteed that the array contains only a single peak element.
+2. Users are expected to solve this in O(log(N)) time. The array may contain duplicate elements.
+
+*** Problem Constraints
+
+1 <= |A| <= 100000
+1 <= A[i] <= 109
+
+*** Input Format
+The only argument given is the integer array A.
+
+*** Output Format
+Return the peak element.
+
+*** Example Input
+
+Input 1:
+A = [1, 2, 3, 4, 5]
+
+Input 2:
+A = [5, 17, 100, 11]
+
+*** Example Output
+Output 1: 5
+Output 2: 100
+
+*** Example Explanation
+
+Explanation 1: 
+5 is the peak.
+
+Explanation 2: 
+100 is the peak.
+
+*/
+
+module.exports = {
+    //param A : array of integers
+    //return an integer
+    solve: function (A) {
+
+        let n = A.length;
+
+        if (n === 1) {
+            return A[0];
+        }
+
+        let start = 0;
+        let end = n - 1;
+
+        while (start <= end) {
+
+            let mid = Math.floor(start + ((end - start) / 2));
+
+            if (mid === 0 && A[mid] > A[mid + 1]) {
+                return A[mid];
+            }
+            else if (mid === n - 1 && A[mid] >= A[mid - 1]) {
+                return A[mid];
+            }
+            else if (A[mid] > A[mid + 1] && A[mid] > A[mid - 1]) {
+                return A[mid];
+            }
+            else if (mid - 1 >= 0 && A[mid] < A[mid - 1]) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+
+        }
+
+        return -1;
+
+    }
+};
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ DSA practice
 Binary Search
 1. Rotated Sorted Array Search - https://www.scaler.com/academy/mentee-dashboard/class/30364/assignment/problems/203/?navref=cl_pb_nv_tb
 2. Sorted Insert Position - https://www.scaler.com/academy/mentee-dashboard/class/30364/assignment/problems/204/?navref=cl_pb_nv_tb
+3. Find a peak element - https://www.scaler.com/academy/mentee-dashboard/class/30364/assignment/problems/4132/?navref=cl_pb_nv_tb
